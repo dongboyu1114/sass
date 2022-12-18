4b2d9995449dbefca63735cf303e240fa00d5340
pushiyu <dongboyu1114@163.com>
2022-12-18 18:34:45 +0800

    登录模块的实现

---
+++ src/utils/request.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { Message } from 'element-ui'
+const service = axios.create({
+  // 当执行npm run dev
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000 // 设置超时时间
+})
+service.interceptors.request.use()
+// 响应拦截器
+service.interceptors.response.use(response => {
+  // axios默认加了一层data
+  const { success, message, data } = response.data
+  // 通过success成功与否决定下面操作
+  if (success) {
+    return data
+  } else {
+    Message.error(message) // 提示错误消息
+    return Promise.reject(new Error(message))
+  }
+}, error => {
+  Message.error(error.message) // 提示错误信息
+  return Promise.reject(error) // 返回执行错误，让当前的执行链跳出成功 直接进入catch
+})
+export default service
