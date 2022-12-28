b7ad4f81dad740045b4df8adc5dca1f6f3b7c916
pushiyu <dongboyu1114@163.com>
2022-12-28 23:45:13 +0800

    完成主页模块

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,23 @@
 import Cookies from 'js-cookie'
 
 const TokenKey = 'hrsaas-ihrm-token'
-
-export function getToken() {
+const timeKey = 'hrsaas-timestamp-key'
+export function getToken () {
   return Cookies.get(TokenKey)
 }
 
-export function setToken(token) {
+export function setToken (token) {
   return Cookies.set(TokenKey, token)
 }
 
-export function removeToken() {
+export function removeToken () {
   return Cookies.remove(TokenKey)
+}
+// 获取时间戳
+export function getTimeStamp () {
+  return Cookies.get(timeKey)
+}
+// 读取时间戳
+export function setTimeStamp () {
+  Cookies.set(timeKey, Date.now())
 }
